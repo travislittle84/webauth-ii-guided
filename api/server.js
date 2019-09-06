e70ef23373e17fc3d158122448bd31ba451f86f1
Travis Little <kingtrav@gmail.com>
2019-09-05 19:11:03 -0600

    done

--- api/server.js
+++ api/server.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const db = require('../database/dbConfig.js')
+const session = require('express-session')
+const connectSessionKnex = require('connect-session-knex')
+
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const KnexSessionStore = connectSessionKnex(session)
+
+const sessionConfig = {
+  name: 'mechanical keyboard life',
+  // THIS SHOULD NOT NORMALLY HARD CODED - should use enviorment variable
+  secret: 'monsoon demons are messing with my gutters',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // in real world should be true
+    httpOnly: true // browser can't access via javascript
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,6 +11,7 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      req.session.user = user
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -25,6 +26,7 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -36,5 +38,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if(error){
+        res.json({ message: 'You can checkout but you cant leave'})
+      } else {
+        res.end()
+      }  
+    })
+  }
+})
 
 module.exports = router;
